1ff74acb7e339ede26b61257fb6f0292734e7073
Maogxer <Maogxer@gmail.com>
2022-06-28 17:04:10 +0800

    v-model 的修饰符：.lazy

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,10 +1,7 @@
 <template>
   <h3> {{ name }} </h3>
-  <!-- <input type="text"
-         v-model="content" /> -->
   <input type="text"
-         :value="content"
-         @input="content = $event.target.value" />
+         v-model.lazy="content" />
   <span>→ Content: {{ content }}</span>
 </template>
 
