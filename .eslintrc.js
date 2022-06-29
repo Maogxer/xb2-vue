4b1e8ad2295cd6da0495b4d302755cfef6066606
Maogxer <Maogxer@gmail.com>
2022-06-29 20:35:28 +0800

    'vue/multi-word-component-names': 'off'

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
@@ -12,7 +12,8 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'vue/multi-word-component-names': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-}
+};
