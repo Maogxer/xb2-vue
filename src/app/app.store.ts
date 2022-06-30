9241ff33416fcba90ddc40b2db6a74742668e383
Maogxer <Maogxer@gmail.com>
2022-06-30 21:37:17 +0800

    在 Vue 应用里创建与使用 Store

---
+++ src/app/app.store.ts
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex';
+
+/**
+ * 创建 Store
+ */
+const store = createStore({});
+
+/**
+ * 导出 Store
+ */
+export default store;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
+import appStore from './app/app.store';
 import App from './app/app.vue';
 
 /**
  * 创建应用
  */
 const app = createApp(App);
+
+/**
+ * 使用 Store
+ */
+app.use(appStore);
 
 /**
  * 挂载应用
