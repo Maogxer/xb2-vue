f07e50e0fbdb3ae3d11a302bd1554d2e2d078c9c
Maogxer <Maogxer@gmail.com>
2022-07-06 21:32:06 +0800

    安装与使用 HTTP 客户端请求服务端接口（axios）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,17 +1,32 @@
 <template>
   <h3> {{ name }} </h3>
+  <div v-for="post in posts"
+       :key="post.id">
+    {{post.title}} -
+    <small>{{post.user.name}}</small>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data () {
     return {
       name: 'NINGHAO',
+      posts: []
     };
+  },
+
+  created () {
+    axios.get('http://127.0.0.1:3000/posts').then(response => {
+      console.log(response);
+      this.posts = response.data;
+    })
   }
 }
 </script>
-
+ 
 <style scoped>
 @import './styles/app.css';
 </style>
