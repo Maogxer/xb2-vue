c49e41503ec1285f1d8d14b68720bb710fe4f5b2
Maogxer <Maogxer@gmail.com>
2022-07-06 09:09:03 +0800

    声明具有反应特性的数据（reactive）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,17 +1,26 @@
 <template>
   <h3 @click="name='宁皓网'"> {{ name }} </h3>
+  <div @click="user.name='WANGHAO'">{{ user.name }}</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export default {
   setup () {
     const name = ref('NINGHAO');
+
+    const user = reactive({
+      name: '王皓'
+    });
+
     console.log(name.value);
 
+    console.log(user);
+
     return {
-      name
+      name,
+      user
     }
   }
 }
