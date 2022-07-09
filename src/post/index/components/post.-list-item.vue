c087a60ffc333dcd68c212f131facf01435b9ae4
Maogxer <Maogxer@gmail.com>
2022-07-09 20:56:12 +0800

    在 Vue 组件内部请求服务接口获取组件需要的数据

--- src/post/index/components/post.-list-item.vue
+++ src/post/index/components/post.-list-item.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>PostListItem</div>
+  <h3>{{ item.title }} - <small>{{ item.user.name }}</small></h3>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    item: Object
+  }
+});
 </script>
